2b99b6a0c8bde6fe3ab0133a6f42b93d7fdc8429
nikhilyadv <nikhilk.yadav22@gmail.com>
2019-02-11 13:57:06 +0530

    Schema Updated

--- Schema.sql
+++ Schema.sql
@@ -1,11 +1,16 @@
-show tables;
+create table Users (
+  username VARCHAR (20) primary key not null,
+  passcode VARCHAR (20) not null,
+  roles VARCHAR (20) not null
+);
 
 create table customer (
   customer_id VARCHAR (20) primary key not null,
   name VARCHAR (20) not null,
   address VARCHAR (60) not null,
   phone_number DECIMAL (10) UNSIGNED not null,
-  email_id VARCHAR (20) not null
+  email_id VARCHAR (20) not null,
+  foreign key (customer_id) references Users(username) on delete cascade
 );
 
 create table payment (
@@ -17,7 +22,6 @@ create table payment (
 
 create table order_ (
   order_id VARCHAR (20) primary key not null,
-  total_price INT not null,
   customer_id VARCHAR (20),
   shipping_address varchar(60) not null,
   payment_id VARCHAR (20),
@@ -30,18 +34,21 @@ create table supplier (
   name varchar (20) not null,
   address varchar (60) not null,
   phone_number decimal (10) UNSIGNED NOT NULL,
-  email_id VARCHAR (20) not null
+  email_id VARCHAR (20) not null,
+  foreign key (supplier_id) references Users(username) on delete cascade
 );
 
 create table shipper (
   shipper_id varchar (20) primary key not null,
   name varchar (20) not null,
   head_quarters varchar (60) not null,
-  phone_number decimal (10) UNSIGNED not null
+  phone_number decimal (10) UNSIGNED not null,
+  email_id VARCHAR (20) not null,
+  foreign key (shipper_id) references Users(username) on delete cascade
 );
 
 create table track (
-  index_ INT primary key not null,
+  index_ INT AUTO_INCREMENT primary key not null,
   shipper_id varchar (20),
   tracking_id varchar (20),
   foreign key (shipper_id) references shipper (shipper_id) on delete set null
@@ -50,6 +57,7 @@ create table track (
 create table product (
   product_id varchar (20) not null,
   supplier_id varchar (20) not null,
+  price float not NULL,
   total_stock int,
   description varchar (60),
   foreign key (supplier_id) references supplier (supplier_id) on delete cascade,
@@ -72,3 +80,22 @@ create table product_order (
   foreign key (supplier_id) references supplier (supplier_id) on delete set null,
   foreign key (ship_index) references track (index_) on delete set null
 );
+
+INSERT INTO Users Values ("Nikhil", "lol", "CUS");
+INSERT INTO Users Values ("Sourabh", "foo", "SUP");
+INSERT INTO Users Values ("FEDEx", "goo", "SHI");
+
+INSERT INTO customer Values ("Nikhil","Nikhil Kumar","ROOM-119",8281112705,"111601013@");
+INSERT INTO supplier Values ("Sourabh","Sourabh Agg","ROOM-211",8281112700,"111601025@");
+INSERT INTO shipper Values ("FEDEx","FEDEx","Delhi",1800123343,"111601020@");
+
+INSERT INTO product Values ("1","Sourabh",10,100.0,"Rasgulla from Aggarwal Sweets");
+
+INSERT INTO payment Values ("1","4362536563578",NULL,"CompLabFF");
+
+INSERT INTO order_ Values ("1","Nikhil","RM-119","1");
+
+INSERT INTO track(shipper_id) Values ("FEDEx");
+
+INSERT INTO product_order Values ("1","1","Sourabh",5,4,1,NULL,NULL,10);
+
